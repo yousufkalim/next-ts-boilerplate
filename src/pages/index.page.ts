f5014df4b1019ced295ad0dc620d5462f45109bc
Yousuf Kalim <yousufkalim@outlook.com>
2023-11-15 18:48:04 +0500

    Add sample getStaticProps

--- src/pages/index.page.ts
+++ src/pages/index.page.ts
@@ -1,1 +1,11 @@
 export { Home as default } from '@layouts/Home';
+
+export function getStaticProps() {
+  const title = 'Home Page';
+  const description = 'Website created using Next.js boilerplate by Yousuf Kalim';
+  const author = 'Yousuf Kalim';
+
+  return {
+    props: { title, description, author },
+  };
+}
